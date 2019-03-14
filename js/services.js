07b346019808ec3b7eca86f5bf2ba87950d02622
Christian Septimo <clowreed@gmail.com>
2019-03-14 16:05:17 +0800

    Added Manaty research page

--- js/services.js
+++ js/services.js
@@ -13,12 +13,14 @@ $(function() {
                 $(".service-icons-container .icon[data-title!='" + $(this).data('title') + "']").removeClass('active');
                 $(".service-icons-container .icon[data-title='" + $(this).data('title') + "']").addClass('active');
 
-                $(".service-icons-container[data-title='" + $(this).data('title') + "']").addClass('active');
-                $(".service-icons-container[data-title!='" + $(this).data('title') + "']").removeClass('active');
+                if($(this).data('title') !== 'Scientific Research' && $(this).data('title') !== 'Recherche Scientifique') {
+                  $(".service-icons-container[data-title='" + $(this).data('title') + "']").addClass('active');
+                  $(".service-icons-container[data-title!='" + $(this).data('title') + "']").removeClass('active');
 
-                $('#service-competencies-title').html(replaceWithApostrophe($(this).data("title")));
-                $('#service-competencies-description').html($(this).data('description'))
-                $(".service-icons-description-container").addClass('active')
+                  $('#service-competencies-title').html(replaceWithApostrophe($(this).data("title")));
+                  $('#service-competencies-description').html($(this).data('description'))
+                  $(".service-icons-description-container").addClass('active')
+                }
             }
 
 
